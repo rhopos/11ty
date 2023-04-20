33e214ae61f5d06eb9b5291614a43621e2ed3a98
rhopos <kthopos@gmail.com>
2023-04-20 20:37:25 +0300

    .

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	language: "uk",
 	description: "Перелік гуманітарних центрів, громадських організацій, райвиконкомів та інших місць де можна отримати соціальну та гуманітарну допомогу для ВПО в Кривому Розі",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Rhopos",
+		email: "kthopos@hey.com",
 		url: "https://vpo.wiki/about/"
 	}
 }
---
+++ content/pokrov.md
@@ -0,0 +1,11 @@
+---
+title: Покровський район. Гуманітарні центри
+description: This is a post on My Blog about leveraging agile frameworks.
+---
+
+<h1>Tagged “{{ tag }}”</h1>
+
+{% set postslist = collections[ tag ] %}
+{% include "postslist.njk" %}
+
+<p>See <a href="/tags/">all tags</a>.</p>
